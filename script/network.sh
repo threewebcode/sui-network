70bf591270d7f0908fde45141430842a7b922eb5
threewebcode <magestore@outlook.com>
2023-03-29 04:28:29 +0000

    feat: sui network

--- script/network.sh
+++ script/network.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 function sui(){
-  git clone https://github.com/MystenLabs/sui.git
-  cd sui
-  cargo build --bin sui-test-validator --bin sui
+    git clone https://github.com/MystenLabs/sui.git
+    cd sui
+    cargo build --bin sui-test-validator --bin sui
 }
 
 $@
